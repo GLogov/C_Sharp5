5d3f2875ebfdf0a51f4b01ba63965cf21cd6b5f6
Silent <silent3_3@mail.ru>
2023-04-12 17:38:40 +0300

    Внесены изменения в задачу 38

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ double result = FindMaxArrayElement(array) - FindMinArrayElement(array);
 Console.WriteLine($"Разница между максимальным и минимальным элементами = {result}");
 
 
-double FindMaxArrayElement(double[] array)
+double FindMaxArrayElement(double[] array) // Находит максимальный элемент массива
 {
     double max = array[0];
     for (int i = 0; i < array.Length; i++)
@@ -22,7 +22,7 @@ double FindMaxArrayElement(double[] array)
     return max;
 }
 
-double FindMinArrayElement(double[] array)
+double FindMinArrayElement(double[] array) // Находит минимальный элемент массива
 {
     double min = array[0];
     for (int i = 0; i < array.Length; i++)
@@ -35,18 +35,23 @@ double FindMinArrayElement(double[] array)
     return min;
 }
 
-double[] GenerateArray(int length) // Генерирует рандомный массив указанной длинны
+double[] GenerateArray(int length) // Генерирует рандомный массив чисел с двумя знаками после запятой указанной длинны
 {
     double[] array = new double[length];
     Random random = new Random();
     for (var i = 0; i < length; i++)
     {
-        array[i] = random.Next(1, 10);
+        array[i] = random.Next(101, 1000);
+        array[i] /= 100;
     }
     return array;
 }
 
 void PrintArray(double[] array) // Печатает массив
 {
-    Console.WriteLine($"[{string.Join(", ", array)}]");
+    Console.WriteLine($"[{string.Join("; ", array)}]");
 }
+
+
+
+
